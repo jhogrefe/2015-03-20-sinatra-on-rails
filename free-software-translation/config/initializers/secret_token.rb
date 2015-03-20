fddd63fddbec2fa14155752623a9c0f7ee575d1c
Jason Hogrefe <hogrefejason@gmail.com>
2015-03-20 11:17:47 -0500

    First commit. Created Rails file and deleted username and password from yml file.

---
+++ free-software-translation/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreeSoftwareTranslation::Application.config.secret_token = '4b73c3fb65743dcde8cea3c4e3ecbf5e1f5f6d795b30e61ba91b6f8d9da1a731388dfe8e8785bbf508e04cb8a7c60c765378baf76dbc2c6f845022b7741994dd'
